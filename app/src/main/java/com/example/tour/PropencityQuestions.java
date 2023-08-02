4cfa2079f39872cc85526fba9a71f425a4d6b399
KimHyeongJoong <fomula91@gmail.com>
2023-08-03 05:12:46 +0900

    feat: 여행성향 질문리스트 새로운 뷰를 작성했습니다

---
+++ app/src/main/java/com/example/tour/PropencityQuestions.java
@@ -0,0 +1,70 @@
+package com.example.tour;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.gson.Gson;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class PropencityQuestions extends AppCompatActivity {
+    private int[] answers;
+    private List<Integer> questionIds;
+
+    @Override
+    protected void onCreate(Bundle saveInstanceState){
+        super.onCreate(saveInstanceState);
+        setContentView(R.layout.activity_propencity_travel);
+
+        List<Question> questions = loadQuestionsFromGson();
+        Log.d("questions", questions.toString());
+        LinearLayout linearLayout = findViewById(R.id.question_linearlayout);
+
+        //List<String> options = new ArrayList<>(Arrays.asList("매우그렇다","그렇다","보통이다","그렇지않다","매우그렇지않다"));
+        String[] options = {"매우그렇다","그렇다","보통이다","그렇지않다","매우그렇지않다"};
+        for(Question question : questions) {
+            TextView textView = new TextView(this);
+            textView.setText(question.getText());
+            linearLayout.addView(textView);
+
+            RadioGroup radioGroup = new RadioGroup(this);
+            radioGroup.setOrientation(RadioGroup.HORIZONTAL);
+            for(int i = 0; i < options.length; i++) {
+                RadioButton radioButton = new RadioButton(this);
+                radioButton.setText(options[i]);
+                radioGroup.addView(radioButton);
+            }
+            linearLayout.addView(radioGroup);
+        }
+
+    }
+    private List<Question> loadQuestionsFromGson() {
+        List<Question> questions = new ArrayList<>();
+        try {
+            InputStream is = getAssets().open("question.json");
+            Reader reader = new InputStreamReader(is, "UTF-8");
+
+            Gson gson = new Gson();
+            questions = Arrays.asList(gson.fromJson(reader, Question[].class));
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return questions;
+    };
+}
+
+
---
+++ app/src/main/java/com/example/tour/Question.java
@@ -0,0 +1,15 @@
+package com.example.tour;
+
+
+public class Question {
+    private int id;
+    private String text;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getText() {
+        return text;
+    }
+}

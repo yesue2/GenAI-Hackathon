bb6ed42494128d4cf3b93ad4f9a099985abb1e97
KimHyeongJoong <fomula91@gmail.com>
2023-08-04 23:54:21 +0900

    feat: 다음 여행지 추천에서 텍스트 내용들이 랜덤하게 출력되도록 코드를 수정하였습니다.

---
+++ app/src/main/java/com/example/tour/NextTrip.java
@@ -0,0 +1,12 @@
+package com.example.tour;
+
+public class NextTrip {
+
+    private int id;
+    private String travel;
+    private String description;
+    public int getId() {return id;}
+    public String getTravel() {return travel;}
+    public String getDescription() {return description;}
+
+}

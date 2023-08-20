525902ddf528d9967bf722d04bc28a6141a5d86e
yeseu2 <ysbjh2464@naver.com>
2023-08-20 16:46:20 +0900

    - API 주소 숨기기

---
+++ app/src/main/java/com/example/tour/data/BuildConfig.java
@@ -0,0 +1,11 @@
+package com.example.tour.data;
+
+public final class BuildConfig {
+    public static final boolean DEBUG = Boolean.parseBoolean("true");
+    public static final String APPLICATION_ID = "com.example.tour.data";
+    public static final String BUILD_TYPE = "debug";
+    public static final int VERSION_CODE = 1;
+    public static final String VERSION_NAME = "1.0";
+
+    public static final String SERVER = "https://t-api-play.actionfriends.net/api/v1/hackathon2023/";
+}
